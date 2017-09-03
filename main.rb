11bc4455d964e6dc81b452d22dae55435e83cb32
Tomas Korcak <korczis@gmail.com>
2017-09-03 22:50:49 +0200

    Rozdeleni castky na mnozstvi a menu

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
+# encoding: utf-8
+
 require 'csv'
 require 'nokogiri'
 require 'open-uri'
@@ -9,13 +11,17 @@ URL = 'https://www.fio.cz/ib2/transparent?a=2501277007'.freeze
 FIELDS = %w(datum castka typ protiucet zprava ks vs ss poznamka).freeze
 
 def main
-  page = Nokogiri::HTML(open(URL))
+  page = Nokogiri::HTML(open(URL), nil, 'UTF-8')
 
   data = page.css('div.content > table > tbody > tr').map do |element|
     res = Hash.new(0)
     FIELDS.each_with_index do |name, idx|
       res[name] = element.css("td[#{idx + 1}]").text.strip
     end
+    castka = res['castka']
+    parts = castka.split("\u00A0")
+    res['mnozstvi'] = parts[0].to_f
+    res['mena'] = parts[1]
     res
   end
 
